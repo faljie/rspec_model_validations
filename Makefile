faljie/ryquest

master - init repos

Makefile
@@ -1,0 +1,45 @@
+##
+# Simplify docker usage
+#
+container = gem_ryquest
+option = -ti --rm --user $(shell id -u):$(shell id -g) -v $(shell pwd):/app -e HOME=/app
+
+.PHONY: doc
+args = $(filter-out $@,$(MAKECMDGOALS))
+%:
+	@:
+
+# build the docker image
+install:
+	@ docker build -t $(container) .
+
+# run the test inside container
+test:
+	@ docker run ${option} ${container} rspec $(args)
+
+# display linter error
+lint:
+	@ docker run $(option) ${container} rubocop ${args}
+
+# documentation compilation
+doc:
+	@ docker run $(option) $(container) yardoc --list-undoc $(args)
+
+# exec the commande in the container: make exec bash
+exec:
+	@ docker run $(option) $(container) $(args)
+
+# remove everything created by makefile command
+clean:
+	- rm -rf doc
+	- rm -rf coverage
+	- rm -rf .bundle
+	- rm -rf .yardoc
+	- rm -rf .cache
+	- rm -rf .local
+	- rm .rspec_status
+	- rm .byebug_history
+	- rm .irb_history
+	- rm .bash_history
+	- docker image rm ruby:3.0.2
+	- docker image rm $(container)

lib/ryquest/version.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+# Root module
+module Ryquest
+  # Ryquest version number: major.minor.maintenance
+  VERSION = '0.1.0'
+end

spec/spec_helper.rb
@@ -1,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'byebug'
+
+require 'simplecov'
+
+SimpleCov.start
+
+require 'ryquest'
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end

faljie/rspec_model_validations

Makefile
@@ -1,0 +1,45 @@
+##
+# Simplify docker usage
+#
+container = gem_rspec_model_validations
+option = -ti --rm --user $(shell id -u):$(shell id -g) -v $(shell pwd):/app -e HOME=/app
+
+.PHONY: doc
+args = $(filter-out $@,$(MAKECMDGOALS))
+%:
+	@:
+
+# build the docker image
+install:
+	@ docker build -t $(container) .
+
+# run the test inside container
+test:
+	@ docker run ${option} ${container} rspec $(args)
+
+# display linter error
+lint:
+	@ docker run $(option) ${container} rubocop ${args}
+
+# documentation compilation
+doc:
+	@ docker run $(option) $(container) yardoc --list-undoc $(args)
+
+# exec the commande in the container: make exec bash
+exec:
+	@ docker run $(option) $(container) $(args)
+
+# remove everything created by makefile command
+clean:
+	- rm -rf doc
+	- rm -rf coverage
+	- rm -rf .bundle
+	- rm -rf .yardoc
+	- rm -rf .cache
+	- rm -rf .local
+	- rm .rspec_status
+	- rm .byebug_history
+	- rm .irb_history
+	- rm .bash_history
+	- docker image rm ruby:3.0.2
+	- docker image rm $(container)

lib/rspec_model_validations/version.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+# Root module
+module RspecModelValidations
+  # RspecModelValidations version number: major.minor.maintenance
+  VERSION = '0.1.0'
+end

spec/spec_helper.rb
@@ -1,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'byebug'
+require 'simplecov'
+
+SimpleCov.start
+
+require 'rspec_model_validations'
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end