5b2b60ea4bcf498de60175db6294a607727fdef0
faljie <pierre.guego@champagne-terroir.fr>
2022-11-02 14:12:18 +0000

    1.0.0 - implement invalidate with option

--- lib/rspec_model_validations/matchers/invalidate.rb
+++ lib/rspec_model_validations/matchers/invalidate.rb
@@ -5,6 +5,16 @@
 class RspecModelValidations::Matchers::Invalidate
   include RspecModelValidations::Matchers::Base
 
+  def initialize attribute
+    super
+
+    @with = nil
+  end
+
+  # Set the with option
+  # @param errors [*Symbol] One or more error which should be in attribute errors
+  def with *errors; @with = errors end
+
   # Run the test
   # @param model[ActiveModel::Model]
   # @return [Boolean] Test result
@@ -15,22 +25,39 @@ class RspecModelValidations::Matchers::Invalidate
     @model = model
     @errors = attribute_errors model
 
-    @errors.count != 0
+    @errors.count != 0 && (@with.nil? || (@with & @errors) == @with)
   end
 
   # Explain why matches? fail
   # @return [String]
   def failure_message
-    message "#{attribute_value(@model).inspect} to be an invalid value for #{@model.class}##{@attribute}", @errors
+    attribute = "#{@model.class}##{@attribute}"
+    value = attribute_value(@model).inspect
+
+    expected = "#{value} to be an invalid value for #{attribute}"
+    expected = "#{attribute} to invalidate #{value} with #{@with.map(&:inspect).join ', '}" unless @with.nil?
+
+    message expected, @errors
   end
 
   # Explain why matches? fail when negated
   # @return [String]
   def failure_message_when_negated
-    message "#{attribute_value(@model).inspect} to be a valid value for #{@model.class}##{@attribute}", @errors
+    attribute = "#{@model.class}##{@attribute}"
+    value = attribute_value(@model).inspect
+
+    expected = "#{value} to be a valid value for #{attribute}"
+    expected = "#{attribute} not to invalidate #{value} with #{@with.map(&:inspect).join ', '}" unless @with.nil?
+
+    message expected, @errors
   end
 
   # One line syntax description
   # @return [String]
-  def description; "invalidate #{@attribute.inspect} attribute" end
+  def description
+    result = "invalidate #{@attribute.inspect} attribute"
+    result = "#{result} with #{@with.map(&:inspect).join ', '}" unless @with.nil?
+
+    result
+  end
 end
--- spec/rspec_model_validations/matchers/invalidate_spec.rb
+++ spec/rspec_model_validations/matchers/invalidate_spec.rb
@@ -49,6 +49,33 @@ RSpec.describe RspecModelValidations::Matchers::Invalidate do
       end
     end
 
+    context 'when option with is set' do
+      let(:model) do
+        build_dummy do
+          attr_accessor :target
+
+          validates :target, presence: true, numericality: { greater_than: 9 }
+        end.new target: 12
+      end
+      before { instance.with :blank, :not_a_number }
+
+      context 'when target have not errors' do
+        it { should be_falsy }
+      end
+
+      context 'when target have not all the with errors' do
+        before { model.target = 'invalid' }
+
+        it { should be_falsy }
+      end
+
+      context 'when target have the with errors' do
+        before { model.target = nil }
+
+        it { should be_truthy }
+      end
+    end
+
     context 'when target is not a model attribute' do
       let(:instance) { described_class.new :unknow }
 
@@ -63,6 +90,12 @@ RSpec.describe RspecModelValidations::Matchers::Invalidate do
     subject { instance.failure_message }
 
     it { should eq "\nexpected: :valid to be an invalid value for #{model.class}#target\n     got: []" }
+
+    context 'when option with is set' do
+      before { instance.with :blank }
+
+      it { should eq "\nexpected: #{model.class}#target to invalidate :valid with :blank\n     got: []" }
+    end
   end
 
   describe '#failure_message_when_negated' do
@@ -73,11 +106,23 @@ RSpec.describe RspecModelValidations::Matchers::Invalidate do
     subject { instance.failure_message_when_negated }
 
     it { should eq "\nexpected: nil to be a valid value for #{model.class}#target\n     got: [:blank]" }
+
+    context 'when option with is set' do
+      before { instance.with :blank }
+
+      it { should eq "\nexpected: #{model.class}#target not to invalidate nil with :blank\n     got: [:blank]" }
+    end
   end
 
   describe '#description' do
     subject { instance.description }
 
     it { should eq 'invalidate :target attribute' }
+
+    context 'when option with is set' do
+      before { instance.with :blank, :not_a_number }
+
+      it { should eq 'invalidate :target attribute with :blank, :not_a_number' }
+    end
   end
 end
