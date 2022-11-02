f0507af679ca627b2b59277f3ff3a3704d8a064e
faljie <pierre.guego@champagne-terroir.fr>
2022-11-02 15:41:55 +0000

    1.0.0 - add matcher allias

--- lib/rspec_model_validations/matchers.rb
+++ lib/rspec_model_validations/matchers.rb
@@ -1,3 +1,11 @@
-# Hold 'alias' to run matchers
+# Rspec 'alias' to run matchers.
+# Include with rspec configuration method `include`
 module RspecModelValidations::Matchers
+  # `validate` alias
+  # @return [Validate]
+  def validate attribute; Validate.new attribute end
+
+  # `invalidate` alias
+  # @return [Validate]
+  def invalidate attribute; Invalidate.new attribute end
 end
